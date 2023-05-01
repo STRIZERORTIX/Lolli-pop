b1872329a88ff081d3ecd390b5b958a149b769a3
STRIZERORTIX <abhi.varadan@gmail.com>
2023-05-01 21:01:30 +0530

    added backed server

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
         const fetchPosts = async () => {
             setLoading(true);
             try{
-                const response = await fetch('http://localhost:8080/api/v1/post' , {
+                const response = await fetch('https://lollipop.onrender.com/api/v1/post' , {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
