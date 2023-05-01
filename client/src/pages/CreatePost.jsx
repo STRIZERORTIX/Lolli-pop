a52e5cae0bbaa915c037a1a79bc149f690d34a6c
STRIZERORTIX <abhi.varadan@gmail.com>
2023-05-01 21:05:55 +0530

    changed to lollipop

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -81,7 +81,7 @@ const CreatePost = () => {
         <section className="max-w-7xl mx-auto">
             <div>
                 <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-                <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+                <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through Lollipop and share it with the community</p>
             </div>
 
             <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
